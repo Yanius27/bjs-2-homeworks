07c7041e19875b250c471e6e6c6fc2f5da52a9ce
Yanius27 <zhzh-1987@mail.ru>
2023-02-08 01:29:16 +0700

    Четвёртое задание сделано

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+  this.name= name;
+  this.gender= gender;
+  this.age= age;
+  this.marks= [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject= subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if(this.marks) {
+    this.marks.push(...marks);
+  }
+  else return 0;
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(this.marks && this.marks.length > 0) {
+    return this.marks.reduce((acc, mark) => acc + mark, 0) / this.marks.length;
+  }
+  return 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded= reason;
 }
+
+new Student('John', 'male', 26);
+new Student('Mary', 'female', 24);
+new Student('Julia', 'female', 25);
